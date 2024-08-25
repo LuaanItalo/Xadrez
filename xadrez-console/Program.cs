46edbd95bd9099e3339dac2fbff521618d3484e8
Luan Italo <luanitalo4545@gmail.com>
2024-08-25 18:37:48 -0300

    Criando a Classe Posicao

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,14 @@
+﻿using tabuleiro;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Posicao P;
+
+        P = new Posicao(3, 4);
+
+        Console.WriteLine("POsição: " + P);
+    }
+}
