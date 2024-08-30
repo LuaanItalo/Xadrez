77fade83e062df11376e56e79286ac316ec838b4
Luan Italo <luanitalo4545@gmail.com>
2024-08-30 18:55:48 -0300

    Criando a classe PosicaoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -10,11 +10,12 @@ namespace xadrez_console
     {
         private static void Main(string[] args)
         {
+            /*
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
@@ -25,7 +26,17 @@ namespace xadrez_console
             catch (TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
-            }
+            }*/
+
+           PosicaoXadrez pos = new PosicaoXadrez('a', 1 );
+
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.toPosicao());
+
+
+
+
         }
     }
 }
---
+++ xadrez-console/tabuleiro/PosicaoXadrez.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace tabuleiro
+{
+    internal class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linhs)
+        {
+            this.coluna = coluna;
+            this.linha = linhs;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+
+        }
+
+    }
+}
